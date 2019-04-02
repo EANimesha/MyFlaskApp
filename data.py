ef2655d6f0ed9f16c703ddbe612eca3f999dd925
EANimesha <nimeshadilini999@gmail.com>
2019-04-03 04:07:28 +0530

    First stage

---
+++ data.py
@@ -0,0 +1,26 @@
+def Article():
+    articles=[
+        {
+            'id':'1',
+            'title':'Article one',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa adipisci illo reprehenderit, repudiandae iure blanditiis natus doloribus sapiente eius, nulla voluptatum optio officiis magni a eos accusantium voluptas vitae.',
+            'author':'Nimesha Dilini',
+            'create date':'31/03/2019'
+        },
+        {
+            'id':'2',
+            'title':'Article two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa adipisci illo reprehenderit, repudiandae iure blanditiis natus doloribus sapiente eius, nulla voluptatum optio officiis magni a eos accusantium voluptas vitae.',
+            'author':'Dilini',
+            'create date':'31/03/2019'
+        },
+        {
+            'id':'3',
+            'title':'Article three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa adipisci illo reprehenderit, repudiandae iure blanditiis natus doloribus sapiente eius, nulla voluptatum optio officiis magni a eos accusantium voluptas vitae.',
+            'author':'Nimesha',
+            'create date':'31/03/2019'
+        }
+    ]
+
+    return articles     
